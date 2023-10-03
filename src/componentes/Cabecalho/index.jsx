e643b0770367896ffd9633990d494a10d071445f
Vinicius Rodrigues <viniciusrodriguesmuller@gmail.com>
2023-10-03 13:55:24 -0300

    first commit

---
+++ src/componentes/Cabecalho/index.jsx
@@ -0,0 +1,20 @@
+import { styled } from "styled-components"
+import CampoTexto from "../CampoTexto"
+
+const HeaderEstilizado = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    img {
+        max-width: 212px;
+    }
+`
+
+const Cabecalho = ({filtro, setFiltro}) => {
+    return (<HeaderEstilizado>
+        <img src="/imagens/logo.png" alt="" />
+        <CampoTexto filtro={filtro} setFiltro={setFiltro} />
+    </HeaderEstilizado>)
+}
+
+export default Cabecalho
---
+++ src/componentes/Rodape/index.jsx
@@ -0,0 +1,57 @@
+import { styled } from 'styled-components';
+import {BsInstagram, BsTwitter, BsFacebook} from 'react-icons/bs'
+
+
+const RodapeEstilizado = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    margin-top: 100px;
+    background-color: #04244F;
+    padding: 22px;
+    box-sizing: border-box;
+`;
+
+const IconeContainer = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    li {
+        display: inline-block;
+        margin-right: 32px;
+    }
+`;
+
+const RodapeTexto = styled.p`
+    font-size: 16px;
+    color: white;
+    margin: 0;
+`;
+
+function Rodape() {
+    return (
+        <RodapeEstilizado>
+            <IconeContainer>
+                <li>
+                    <a href="#">
+                        <BsFacebook color='#ffffff' size={20}/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <BsTwitter color='#ffffff' size={20}/>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <BsInstagram color='#ffffff' size={20}/>
+                    </a>
+                </li>
+            </IconeContainer>
+            <RodapeTexto>Desenvolvido por Alura.</RodapeTexto>
+        </RodapeEstilizado>
+    );
+}
+
+export default Rodape;
